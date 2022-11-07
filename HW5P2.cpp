f9d10ef92cf6ada648f44bb9fea74555bc94aa49
Fahad Faruqi <fahadfaruqi1@gmail.com>
2022-11-07 13:44:38 -0500

    test

--- HW5P2.cpp
+++ HW5P2.cpp
@@ -17,7 +17,8 @@ void print(node * head){
         cout << temp->model << endl;
         temp = temp->next;
     }
-}
+} 
+
 
 void removeNode(node* head){
     node * temp = head;
@@ -41,6 +42,7 @@ int main (){ // this was much harder than i thought it would be
 // this is like my linked list in part 1 but reversed
     node *head =NULL;
     node *temp;
+    int martin;
     
     for (int i =0; i < 9; i++){        
         head = new node;
