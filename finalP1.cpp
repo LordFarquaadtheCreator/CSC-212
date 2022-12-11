aaa8ba8cfdca5212eee7ce4eb546715b1044982b
Fahad <fahadfaruqi1@gmail.com>
2022-12-11 13:48:13 -0500

    final done

--- finalP1.cpp
+++ finalP1.cpp
@@ -1,40 +1,42 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-const int YEAR(8), MONTH(12);
+const int YEAR(8), MONTH(12); //default global vars
 
 
-void populate(int *ptr[8], int col = YEAR, int row = MONTH){
+void populate(int *ptr[], int col = YEAR, int row = MONTH){
     unsigned int RV;
     
 
     for (int i = 0; i < col; i++){ // goes through years
         for (int j = 0; j < row; j++){
-            RV = rand()%60; // sets up RV -> normalizes it
-            ptr[i][j] = RV; // assigns random number
+            ptr[i][j] = rand()%60; // assigns random number
         }
     }
 }
 
-void YearAvrg (int *ptr[8], int col = YEAR, int row = MONTH){
+void YearAvrg (int *ptr[], int col = YEAR, int row = MONTH){
     cout << "************* YEARLY AVERAGE *************\n";
     float x = 0;
     for (int i = 0; i < col; i++){//goes through years
         for (int j = 0; j < row; j++){ // goes through months
             x+= ptr[i][j];
         }
-        printf("Year %i Rain Total Average: \t%.2fL\n", i+1, x/row);
-        x=0;
+        printf("Year %i Rain Total Average: \t%.2fL\n", i+1, x/row);// at the end of each year the average is printed
+        x=0; // reset sum
     }
 }
 
-void MonthAvrg (int *ptr[8], int col = YEAR, int row = MONTH){
+void MonthAvrg (int *ptr[], int col = YEAR, int row = MONTH){
+    float sum;
     cout << "************* MONTHLY AVERAGE *************\n";
-    for (int i = 0; i < col; i++){
-        for (int j = 0; j < row; j++){
-            printf("Year %i Month %i Rain Total Average: %i\n", i+1, j+1, ptr[i][j]);
+    for (int j = 0; j < row; j++){ // month
+        for (int i = 0; i < col; i++){ // year
+            sum += ptr[i][j]; // adding each month's val throughout the years
         }
+        printf("Month %i Rain Total Average: %.2f\n", j+1, sum/col);
         cout << "******************************************\n";
+        sum = 0;
     }
 }
 
@@ -54,16 +56,16 @@ int main(){
         mPtr[i] = &M[i][0];
     }
 
-    populate(mPtr);
-    YearAvrg(mPtr);
-    MonthAvrg(mPtr);
+    populate(mPtr,8,12);
+    YearAvrg(mPtr,8,12);
+    MonthAvrg(mPtr,8,12);
 
     float x = 0;
-    cout << "************* YEAR 3 AVERAGE *************\n";
+    cout << "************* YEAR 3 TOTAL *************\n";
     for (int j = 0; j < MONTH; j++){ // goes through every month
         x+= M[2][j]; // index 2 is year 3
     }
-    printf("Year 3 Rain Total Average: \t%.2f L\n", x/MONTH);
+    printf("Year 3 Rain Total: \t%.2f L\n", x);
     cout << "******************************************\n";
 
     return 19;
--- finalP2.cpp
+++ finalP2.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+
 int main (){
    string Name = "Fahad Faruqi";
     cout << "-----------------------------------------\n";
@@ -12,12 +13,9 @@ int main (){
     cout << "FILE: " << __FILE__ << "\tDATE: " << __DATE__ << endl;
     cout << "-----------------------------------------\n";
 
-    ifstream in("sunHem.txt");
+    ifstream in("sunHem.txt"); //opens txt file
 
-    if(!in){
-        cout << "Didn't Open Correctly :/\n";
-        return 1;
-    }
+    if(!in){return 1;} //incase of error -> break
 
     set<string> novel;
     string words;
@@ -25,26 +23,32 @@ int main (){
     while(getline(in, words, ' ')){ // takes in each word that it reads and stores in in the set
         novel.insert(words);
     }
-
-    cout << "Words in the novel:\n ";
-    for (const string& w : novel) {
-        cout << w << "\n";
-    }
-    cout << endl;
-
-    // something may be wrong, the online calculator is giving me 166 unique words 
+    in.close(); // just in case
 
     printf("The total number of unique words in this set are:  %i\n", novel.size()); // size of set == unique elements
+    cout << "******************************************************\n";
 
 
-    int hist[20];
-    for (int i(0); i<20; i++){hist[i]=0;}
-
-    for(const string& w: novel){
-        hist[w.size()]++;
+    int hist[20]; //histogram
+    for (int i(0); i<20; i++){hist[i]=0;} // set all to 0
+    
+    //calculate # of lenghts of words
+    for (const string& word : novel) { //  goes through each word in the set
+        hist[word.length()]++; // increment the value of each word's lenght by 1
     }
+
+    //print "histogram"
     for (int i(0); i < sizeof(hist)/sizeof(hist[0]); i++){
-        printf("Number of Words of Size %i: %i\n", i+1, hist[i]); // something is wrong
+        printf("Number of Words of Size %i: %i\n", i, hist[i]); 
     }
 
+    int upperCase = 0;
+    for (const string& word : novel) {
+    // Check if the first character of the word is an upper-case letter
+        if (word[0] > 64 && word[0] < 92) {
+        upperCase++;
+        }
+    }
+    cout << "******************************\nNumber of words that start with an upper-case letter: " << upperCase << endl; // print results
+    return 0;
 }
